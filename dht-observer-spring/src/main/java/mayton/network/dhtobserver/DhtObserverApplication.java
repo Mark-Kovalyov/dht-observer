93745caae104dae27c2c92427de9c972de468603
mayton <mark.kovalyov78@gmail.com>
2020-05-24 00:03:27 +0300

    Add DHT-Observer Spring version

---
+++ dht-observer-spring/src/main/java/mayton/network/dhtobserver/DhtObserverApplication.java
@@ -0,0 +1,13 @@
+package mayton.network.dhtobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhtObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhtObserverApplication.class, args);
+	}
+
+}
---
+++ dht-observer-spring/src/test/java/mayton/network/dhtobserver/DhtObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package mayton.network.dhtobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhtObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
